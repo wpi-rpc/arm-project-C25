54452959ec4ea006fe383e02de9fd23b0a9efc0e
matthew-papesh <papesh.m2@gmail.com>
2025-02-13 01:31:20 -0500

    added initial servo code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <wiringPi.h>
 #include <softPwm.h>
 
+#define SERVO_PIN   5
+
 int main() {
+    // initialize setupt; exit if failure
+    if(wiringPiSetup() == -1) {
+        exit(EXIT_FAILURE);
+    }
+    // create servo on pin; value range [0, 100]
+    softPwmCreate(SERVO_PIN, 0, 100);
+    double angle = 100.0; // angle in range [0, 180]
+    int value = (int)((angle / 180.0) * 100.0); // map angle -> value
     
+    // set value of angle
+    softPwmWrite(SERVO_PIN, value);
+    // end program after setting position
+    std::cout<<"done!\n";
+    delay(1000);
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,24 @@
+# define flags 
+CC = g++ # compiler
+CXX_FLAGS = -std=c++11 -I/usr/local/include # compiling flags
+LD_FLAGS = -L/usr/local/lib -lwiringPi # linking flags
+
+# define files
+TARGET = main # produced executables
+SRC = main.cpp # source files
+OBJ = main.o # object files
+
+# default target
+all: $(TARGET)
+
+# create all executable from lists
+$(TARGET): $(OBJ)
+	$(CC) -o $(TARGET) $(OBJ) $(LD_FLAGS)
+
+# create all objects from lists
+$(OBJ): $(SRC)
+	$(CC) -c $(SRC) $(CXX_FLAGS)
+
+# clean workspace
+clean: 
+	rm -f $(TARGET) $(OBJ)
