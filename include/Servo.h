3e27d9acea32fccd5f51a8dd44f698887973be64
matthew-papesh <papesh.m2@gmail.com>
2025-02-18 21:08:22 +0000

    added time-step motion profiling; feed-forward

--- include/Servo.h
+++ include/Servo.h
@@ -9,7 +9,27 @@ class Servo {
     
     const int PIN; // BCM pin number on the PI
     const int PWM_RANGE[2] = {500, 2500}; // usecs; range of PWM 
+    int servo_position = 0;
     
+    /**
+     * @brief Calculates the time step delay at a given degree position provided the path the servo needs to rotate. 
+     * Times steps are also determined by a desired acceleration
+     * @param initial_position (int) : The specified initial degree position of the servo
+     * @param final_position (int) : The specified final degree position of the servo
+     * @param position (int) : The specified position of the time step. 
+     * @param acceleration (double) : The specified acceleration in [degrees / sec^2]
+     * @param max_time_step (int) : The specified maximum the time step in milliseconds that it can be
+     * @return The calculated time step in milliseconds  
+     */
+    int timeStep(int initial_position, int final_position, int position, double acceleration, int max_time_step); 
+
+    /**
+     * @brief Drives the motor to the specified degrees position. Drives within a given
+     * range of [-90,+90] degrees such that zero degrees is the initial position and positive is counter-clockwise. 
+     * @param degrees (int) : The specified degree position
+     */
+    void position(int degrees);
+
     public: 
 
     /**
@@ -18,14 +38,27 @@ class Servo {
      * @param PIN (const inst) : The specified BCM pin
      */
     Servo(const int PIN);
+    
+    /**
+     * @brief The assigned BCM pin of the servo. 
+     * @return The servo BCM pin.
+     */
     int getPin() const;
 
     /**
-     * @brief Drives the motor to the specified degrees position. Drives within a given
-     * range of [-90,+90] degrees such that zero degrees is the initial position and positive is counter-clockwise. 
-     * @param degrees (int) : The specified degree position
+     * @brief The approximate feed-forward position of the servo in degrees. 
+     * @return The servo position in degrees.
      */
-    void position(int degrees);
+    int getPosition();
+
+    /**
+     * @brief Drives the servo to a new local relative position. Pushing the servo position 
+     * in the direction and distance of the specified degrees. This is a relative local change
+     * in positioning when driving the servo, and as such, the magnitude of degrees should be small. 
+     * @note The specified degrees is clamped in range of [-10, +10] degrees. 
+     * @param degrees (int) : The specified relative change in position
+     */
+    void step(int degrees);
 };
 
 #endif
--- src/Servo.cpp
+++ src/Servo.cpp
@@ -2,7 +2,9 @@
 #include <algorithm>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pigpio.h>
+#include <math.h>
 
 Servo::Servo(const int PIN)
     :   PIN(PIN) {
@@ -13,6 +15,19 @@ int Servo::getPin() const {
     return PIN;
 }
 
+int Servo::getPosition() {
+    return servo_position;
+}
+
+void Servo::step(int degrees) {
+    // clamp step in position to a local change in degrees
+    degrees = std::max(-10, std::min(10, degrees));
+    // drive to local position; assume time is instantaneous. 
+    position(degrees);
+    // assume servo is at new position; update position.
+    servo_position += degrees;
+}
+
 void Servo::position(int degrees) {
     // clamp degrees to mapped range of [0, 180] from [-90, +90]
     degrees = std::max(std::min(degrees + 90, 180), 0);  
@@ -20,4 +35,26 @@ void Servo::position(int degrees) {
     double delta = PWM_RANGE[1] - PWM_RANGE[0];
     int value = (double)PWM_RANGE[0] + ((double)degrees / 180.0) * delta;
     gpioServo(PIN, value);
+}
+
+int Servo::timeStep(int initial_position, int final_position, int position, double acceleration, int max_time_step) {
+    // clamp positions
+    initial_position = std::max(-90, std::min(90, initial_position));
+    final_position = std::max(-90, std::min(90, final_position));
+    // compute distances
+    int distance = std::abs(final_position - initial_position);
+    position = std::abs(position);
+    int time_step_millis = 10;
+
+    // compute time-step by if speeding up or down (based on if position is in 1st or 2nd half of the traveling)
+    if(position < distance) {
+        int acclerating_time_step = 1000 * (1/2) * std::pow(2.0 / (acceleration * distance), 0.5);
+        time_step_millis = acclerating_time_step;
+    }
+    else {
+        int decelerating_time_step = 1000 * (1/2) * std::pow(2.0 / (acceleration * (distance - distance)), 0.5);
+        time_step_millis = decelerating_time_step;
+    }
+
+    return time_step_millis;
 }
